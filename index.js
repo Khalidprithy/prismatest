a9afe72aa7c4553c0bc560d4ed4a4cf822b06657
Khalidprithy <khalidprithy@gmail.com>
2023-06-18 00:46:50 +0600

    api added

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 // Mount user routes
 app.use('/users', usersRouter);
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express.js with Prisma and MongoDB app!');
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
